55a7be85fbd3ec9afa527dfddcd4311ca04fcef6
gxj2003 <2061389208@qq.com>
2023-09-16 01:20:33 +0800

    function

--- src/straightline/slp.cc
+++ src/straightline/slp.cc
@@ -27,6 +27,65 @@ Table *A::PrintStm::Interp(Table *t) const {
   // TODO: put your code here (lab1).
 }
 
+//Exp function
+//IdExp
+int A::IdExp::MaxArgs() const {
+
+}
+
+IntAndTable A::IdExp::Interp(Table *t) const {
+
+}
+//NumExp
+int A::NumExp::MaxArgs() const {
+
+}
+
+IntAndTable A::NumExp::Interp(Table *t) const {
+
+}
+//OpExp
+int A::OpExp::MaxArgs() const {
+
+}
+
+IntAndTable A::OpExp::Interp(Table *t) const {
+
+}
+//EseqExp
+int A::EseqExp::MaxArgs() const {
+
+}
+
+IntAndTable A::EseqExp::Interp(Table *t) const {
+
+}
+
+//ExpList function
+//PairExpList
+int A::PairExpList::MaxArgs() const {
+
+}
+
+int A::PairExpList::NumExps() const {
+
+}
+
+IntAndTable A::PairExpList::Interp(Table *t) const {
+
+}
+//LastExpList
+int A::LastExpList::MaxArgs() const {
+
+}
+
+int A::LastExpList::NumExps() const {
+
+}
+
+IntAndTable A::LastExpList::Interp(Table *t) const {
+
+}
 
 int Table::Lookup(const std::string &key) const {
   if (id == key) {
